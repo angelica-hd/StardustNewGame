7ed93f475bbbc07cbcd4c793ce16caf42205ac18
Angélica Hernández D <angelica.lo333@gmail.com>
2023-12-11 19:23:57 -0300

    fix: reset Game clients array whhen level changes

--- scenes/levels.gd
+++ scenes/levels.gd
@@ -5,14 +5,15 @@ extends Node2D
 var current_level = -1 
 func _ready():
 	if is_multiplayer_authority():
-			cargarNivel()
-			victory_menu.all_ready.connect(cargarNivel)
+		cargarNivel()
+		victory_menu.all_ready.connect(cargarNivel)
 
 func cargarNivel():
 	victory_menu.visible = false
 	current_level += 1
 	for child in level_container.get_children():
 		child.queue_free()
+	Game.arr_clientes = []
 	var level = levels[current_level].instantiate()
 	level_container.add_child(level, true)
 	level.level_ended.connect(level_en)
